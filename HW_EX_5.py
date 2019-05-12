2caf4eac9ec1c32a4f1dde26d7f596e606d98ea6
cooxy <47593207+cooxy@users.noreply.github.com>
2019-05-12 17:26:12 +0200

    Update HW_EX_5.py

--- HW_EX_5.py
+++ HW_EX_5.py
@@ -1,7 +1,15 @@
-a = [5,4,2,16,31,22,334,661,2131,6761,78745845,2222222222]
-db = 0
-for i in range(len(a)):
-    if i!=len(a):
-        if a[i]<a[i+1]:
-            db += 1
-print (db)
+def ascend(ls):
+    f = []
+    if ls[1]>ls[0]:
+        f.append(ls[0])
+    for i in range(len(ls)-1):
+        if ls[i+1]<=ls[i]:
+            continue
+        else:
+            f.append(ls[i+1])
+    print(("{} ({})").format(len(f),f))
+
+
+#main
+lst = [2,3,516,61,6712,321,61,17617173247,3243126236324,11111111111111111111111111111]
+ascend(lst)
